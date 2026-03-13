8571ee637eac961febe619f9a9902373048f882b
CaiCheng-Li <caicheng_li@yahoo.com>
2026-03-12 22:27:44 -0400

    Inital files v2

---
+++ frontend/src/components/PageTitle.tsx
@@ -0,0 +1,5 @@
+export default function PageTitle() {
+  return (
+    <h1 id="title">COP 4331 MERN Stack Demo</h1>
+  );
+}
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Login from '../components/Login.tsx';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <PageTitle />
+      <Login />
+    </div>
+  );
+}
