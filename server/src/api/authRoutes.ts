ac38a0f0d82a7c367d8e272a31c3830593f46bef
JohnVarghese007 <john.vc.2005@gmail.com>
2026-03-19 11:45:24 -0400

    commenting out more unused stuff

--- server/src/api/authRoutes.ts
+++ server/src/api/authRoutes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.post("/login", login);
 router.post("/createaccount", createaccount);
-
+// router.post("/verify-email", verifyEmail);
+// router.post("/google-oauth", googleOAuth);
 
 export default router;
