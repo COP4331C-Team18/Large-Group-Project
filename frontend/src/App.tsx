dacf9e8b487fb8dc0999ae7b45796a9e65f0cdfd
CaiCheng-Li <caicheng_li@yahoo.com>
2026-03-22 13:08:27 -0400

    Login Routing Hot Fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import LandingPage from './pages/LandingPage';
 import { Dashboard } from './pages/Dashboard';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </div>
