79af06b0c6f1aad91740b5b0eb8fbdbd75097269
CaiCheng-Li <caicheng_li@yahoo.com>
2026-03-22 12:35:26 -0400

    Revert "Merge pull request #4 from COP4331C-Team18/dashboard-page"
    This reverts commit f9683d9866f127a07dcfcb987ad87a5194dd7068, reversing
    changes made to acb6654109476c97f74a75f0b9d352c513132642.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,5 @@
 import LandingPage from './pages/LandingPage';
-import { Dashboard } from './pages/Dashboard';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
-  return (
-    <div className="Router">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  return <LandingPage />;
 }
