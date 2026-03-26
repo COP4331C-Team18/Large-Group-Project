164071bbf0c04badb9fc036c30f2e23e89c9e7aa
JohnVarghese007 <john.vc.2005@gmail.com>
2026-03-26 15:57:47 -0400

    lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,32 @@
-import LandingPage from '@/pages/LandingPage';
-import { Dashboard } from '@/pages/Dashboard';
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from '@/pages/LoginPage';
-import SignupPage from '@/pages/SignUpPage';
+import {Suspense, lazy} from 'react';
 import PrivateRoutes from '@/config/privateRoutes';
 
+// Lazy loaded pages
+const LandingPage = lazy(() => import('@/pages/LandingPage'));
+const SignupPage = lazy(() => import('@/pages/SignUpPage'));
+const LoginPage = lazy(() => import('@/pages/LoginPage'));
+const Dashboard = lazy(() => import('@/pages/Dashboard'));
+
+// fallback component for lazy loading
+const Loading = () => <div>Loading...</div>;
 
 export default function App() {
   return (
     <div className="Router">
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* Private Routes needs authentication*/}
-        <Route element={<PrivateRoutes />} >
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
+          {/* Private Routes needs authentication*/}
+          <Route element={<PrivateRoutes />} >
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 
